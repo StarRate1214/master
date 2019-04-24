a6b9ccc771ff948247b19d7738551bc69f11326d
Lrsug <observer@localhost.localdomain>
2019-04-24 20:26:25 +0900

    RuleHeader Ver.1.0

--- HEADER/Rule_header.cpp
+++ HEADER/Rule_header.cpp
@@ -2,8 +2,38 @@
 #include "Rule_header.h"
 #include <iostream>
 #include <string>
+RuleHeader::RuleHeader()
+{
 
-RuleHeader::RuleHeader(const RuleHeader &ref){
+}
+
+RuleHeader::~RuleHeader()
+{
+
+}
+
+RuleHeader::RuleHeader(
+        std::string action,
+        std::string protocols,
+        std::string src_addr,
+        std::string src_port,
+        std::string dir_operator,
+        std::string des_addr,
+        std::string des_port,
+        std::string rule_options)
+{
+    this->action = action;
+    this->protocols = protocols;
+    this->src_addr = src_addr;
+    this->src_port = src_port;
+    this->dir_operator = dir_operator;
+    this->des_addr = des_addr;
+    this->des_port = des_port;
+    this->rule_options = rule_options;
+}
+
+RuleHeader::RuleHeader(const RuleHeader &ref)
+{
     action = ref.action;
     protocols = ref.protocols;
     src_addr = ref.src_addr;
@@ -13,7 +43,9 @@ RuleHeader::RuleHeader(const RuleHeader &ref){
     des_port = ref.des_port;
     rule_options = ref.rule_options;
 }
-RuleHeader& RuleHeader::operator=(const RuleHeader &ref){
+
+RuleHeader& RuleHeader::operator=(const RuleHeader &ref)
+{
     action = ref.action;
     protocols = ref.protocols;
     src_addr = ref.src_addr;
@@ -23,27 +55,36 @@ RuleHeader& RuleHeader::operator=(const RuleHeader &ref){
     des_port = ref.des_port;
     rule_options = ref.rule_options;
 }
-std::string RuleHeader::SetAction(std::string action){
+
+std::string RuleHeader::SetAction(std::string action)
+{
     this->action = action;
 }
-std::string RuleHeader::SetProtocols(std::string protocols){
+std::string RuleHeader::SetProtocols(std::string protocols)
+{
     this->protocols = protocols;
 }
-std::string RuleHeader::SetSrc_addr(std::string src_addr){
+std::string RuleHeader::SetSrcAddr(std::string src_addr)
+{
     this->src_addr = src_addr;
 }
-std::string RuleHeader::SetSrc_port(std::string src_port){
+std::string RuleHeader::SetSrcPort(std::string src_port)
+{
     this->src_port = src_port;
 }
-std::string RuleHeader::SetDir_operator(std::string dir_operator){
+std::string RuleHeader::SetDirOperator(std::string dir_operator)
+{
     this->dir_operator = dir_operator;
 }
-std::string RuleHeader::SetDes_addr(std::string des_addr){
+std::string RuleHeader::SetDesAddr(std::string des_addr)
+{
     this->des_addr = des_addr;
 }
-std::string RuleHeader::SetDes_port(std::string des_port){
+std::string RuleHeader::SetDesPort(std::string des_port)
+{
     this->des_port = des_port;
 }
-std::string RuleHeader::SetRule_options(std::string rule_options){
+std::string RuleHeader::SetRuleOptions(std::string rule_options)
+{
     this->rule_options = rule_options;
 }
--- HEADER/Rule_header.h
+++ HEADER/Rule_header.h
@@ -16,22 +16,31 @@ private:
 public:
     RuleHeader();
     ~RuleHeader();
+    RuleHeader(
+        std::string action,
+        std::string protocols,
+        std::string src_addr,
+        std::string src_port,
+        std::string dir_operator,
+        std::string des_addr,
+        std::string des_port,
+        std::string rule_options);
     RuleHeader(const RuleHeader &ref);
     RuleHeader &operator=(const RuleHeader &ref);
     inline std::string  GetAction(void){return action;}
     inline std::string  GetProtocols(void){return protocols;}
-    inline std::string  GetSrc_addr(void){return src_addr;}
-    inline std::string  GetSrc_port(void){return src_port;}
-    inline std::string  GetDir_operator(void){return dir_operator;}
-    inline std::string  GetDes_addr(void){return des_addr;}
-    inline std::string  GetDes_port(void){return des_port;}
-    inline std::string  GetRule_options(void){return rule_options;}
+    inline std::string  GetSrcAddr(void){return src_addr;}
+    inline std::string  GetSrcPort(void){return src_port;}
+    inline std::string  GetDirOperator(void){return dir_operator;}
+    inline std::string  GetDesAddr(void){return des_addr;}
+    inline std::string  GetDesPort(void){return des_port;}
+    inline std::string  GetRuleOptions(void){return rule_options;}
     std::string     SetAction(std::string action){}
     std::string     SetProtocols(std::string protocols){}
-    std::string     SetSrc_addr(std::string src_addr){}
-    std::string     SetSrc_port(std::string src_port){}
-    std::string     SetDir_operator(std::string dir_operator){}
-    std::string     SetDes_addr(std::string des_addr){}
-    std::string     SetDes_port(std::string des_port){}
-    std::string     SetRule_options(std::string rule_options){}
+    std::string     SetSrcAddr(std::string src_addr){}
+    std::string     SetSrcPort(std::string src_port){}
+    std::string     SetDirOperator(std::string dir_operator){}
+    std::string     SetDesAddr(std::string des_addr){}
+    std::string     SetDesPort(std::string des_port){}
+    std::string     SetRuleOptions(std::string rule_options){}
 };
