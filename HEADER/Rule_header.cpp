3248a7d0906d0e335bdd2bb981c8952ab009e593
jwh <ben3329@naver.com>
2019-04-25 19:18:29 +0900

    Rule_header수정, packet data class 완성, gitignore 추가

--- HEADER/Rule_header.cpp
+++ HEADER/Rule_header.cpp
@@ -1,26 +1,22 @@
-#pragma once
 #include "Rule_header.h"
-#include <iostream>
-#include <string>
+
 RuleHeader::RuleHeader()
 {
-
 }
 
 RuleHeader::~RuleHeader()
 {
-
 }
 
 RuleHeader::RuleHeader(
-        std::string action,
-        std::string protocols,
-        std::string src_addr,
-        std::string src_port,
-        std::string dir_operator,
-        std::string des_addr,
-        std::string des_port,
-        std::string rule_options)
+    std::string action,
+    std::string protocols,
+    std::string src_addr,
+    std::string src_port,
+    std::string dir_operator,
+    std::string des_addr,
+    std::string des_port,
+    std::string rule_options)
 {
     this->action = action;
     this->protocols = protocols;
@@ -44,7 +40,7 @@ RuleHeader::RuleHeader(const RuleHeader &ref)
     rule_options = ref.rule_options;
 }
 
-RuleHeader& RuleHeader::operator=(const RuleHeader &ref)
+RuleHeader &RuleHeader::operator=(const RuleHeader &ref)
 {
     action = ref.action;
     protocols = ref.protocols;
@@ -56,35 +52,35 @@ RuleHeader& RuleHeader::operator=(const RuleHeader &ref)
     rule_options = ref.rule_options;
 }
 
-std::string RuleHeader::SetAction(std::string action)
+void RuleHeader::SetAction(std::string action)
 {
     this->action = action;
 }
-std::string RuleHeader::SetProtocols(std::string protocols)
+void RuleHeader::SetProtocols(std::string protocols)
 {
     this->protocols = protocols;
 }
-std::string RuleHeader::SetSrcAddr(std::string src_addr)
+void RuleHeader::SetSrcAddr(std::string src_addr)
 {
     this->src_addr = src_addr;
 }
-std::string RuleHeader::SetSrcPort(std::string src_port)
+void RuleHeader::SetSrcPort(std::string src_port)
 {
     this->src_port = src_port;
 }
-std::string RuleHeader::SetDirOperator(std::string dir_operator)
+void RuleHeader::SetDirOperator(std::string dir_operator)
 {
     this->dir_operator = dir_operator;
 }
-std::string RuleHeader::SetDesAddr(std::string des_addr)
+void RuleHeader::SetDesAddr(std::string des_addr)
 {
     this->des_addr = des_addr;
 }
-std::string RuleHeader::SetDesPort(std::string des_port)
+void RuleHeader::SetDesPort(std::string des_port)
 {
     this->des_port = des_port;
 }
-std::string RuleHeader::SetRuleOptions(std::string rule_options)
+void RuleHeader::SetRuleOptions(std::string rule_options)
 {
     this->rule_options = rule_options;
 }
--- temp/TCP_UDP.h
+++ temp/TCP_UDP.h
@@ -1,81 +1,6 @@
-#pragma once
 #include <sys/types.h>
-class CEthernet
-{
-private:
-    u_int8_t src_mac[6];
-    u_int8_t dst_mac[6];
-    u_int16_t ether_type;
 
-public:
-    CEthernet();
-    ~CEthernet();
-    CEthernet(u_int8_t src_mac[], u_int8_t dst_mac[], u_int16_t ether_type);
-    CEthernet(const CEthernet &ref);
-    CEthernet &operator=(const CEthernet &ref);
-    inline u_int8_t *getSrcMac() { return src_mac; }
-    inline u_int8_t *getDstMac() { return dst_mac; }
-    inline u_int16_t getType() { return ether_type; }
-    void setSrcMac(u_int8_t src_mac[]);
-    void setDstMac(u_int8_t dst_mac[]);
-    void setEtherType(u_int16_t ether_type);
-};
-
-class CIPv4 : public CEthernet
-{
-private:
-    u_int32_t src_ip;
-    u_int32_t dst_ip;
-    u_int8_t tos;
-    bool more_frag;
-    bool dont_frag;
-    u_int8_t ttl;
-
-public:
-    CIPv4();
-    ~CIPv4();
-    CIPv4(
-        u_int32_t src_ip,
-        u_int32_t dst_ip,
-        u_int8_t tos,
-        bool more_frag,
-        bool dont_frag,
-        u_int8_t ttl);
-    CIPv4(const CIPv4 &ref);
-    CIPv4 &operator=(const CIPv4 &ref);
-    inline u_int32_t getSrcIP() { return src_ip; }
-    inline u_int32_t getDstIP() { return dst_ip; }
-    inline u_int8_t getTos() { return tos; }
-    inline bool getMoreFrag() { return more_frag; }
-    inline bool getDontFrag() { return dont_frag; }
-    inline u_int8_t getTTL() { return ttl; }
-    void setSrcIP(u_int32_t src_ip);
-    void setDstIP(u_int32_t dst_ip);
-    void setTos(u_int8_t tos);
-    void setMoreFrag(bool more_frag);
-    void setDontFrag(bool dont_frag);
-    void setTTL(u_int8_t ttl);
-};
-
-class CICMP : CIPv4
-{
-private:
-    u_int8_t icmp_type;
-    u_int8_t icmp_code;
-
-public:
-    CICMP();
-    ~CICMP();
-    CICMP(u_int8_t icmp_type, u_int8_t icmp_code);
-    CICMP(const CICMP &ref);
-    CICMP &operator=(const CICMP &ref);
-    inline u_int8_t getICMPtype() { return icmp_type; }
-    inline u_int8_t getICMPcode() { return icmp_code; }
-    void setICMPtype(u_int8_t icmp_type);
-    void setICMPcode(u_int8_t icmp_code);
-};
-
-class CTCP : CIPv4
+class CTCP
 {
 private:
     u_int16_t src_port;
@@ -134,7 +59,7 @@ public:
     void setWinSize(u_int16_t win_size);
 };
 
-class CUDP : CIPv4
+class CUDP
 {
 private:
     u_int16_t src_port;
