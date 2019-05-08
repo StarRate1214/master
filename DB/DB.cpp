3cd16d9d0cca5ba010393f93f8c216414aedeefb
jwh <ben3329@naver.com>
2019-05-08 21:28:44 +0900

    change preparedstatement, log to event, get eid

--- DB/DB.cpp
+++ DB/DB.cpp
@@ -1,32 +1,83 @@
 #include "DB.h"
 
-CDB::CDB(sql::SQLString hostName, sql::SQLString userName, sql::SQLString password, sql::SQLString dbName)
-    : m_driver(0), m_conn(0), m_statement(0)
+CDB::CDB(sql::SQLString hostName, sql::SQLString userName, sql::SQLString password, sql::SQLString dbName)//생성자
+    : m_driver(0), m_conn(0),
 {
-    this->m_hostName = hostName;
-    this->m_userName = userName;
-    this->m_password = password;
-    this->m_dbName = dbName;
+    //this->m_hostName = hostName;
+    //this->m_userName = userName;
+    //this->m_password = password;
+    //this->m_dbName = dbName;
+    m_driver = sql::mysql::get_driver_instance();
+    m_conn = m_driver->connect(hostName, userName, password);
+    m_conn->setSchema(dbName);
+    m_strEvent.reset(m_conn->prepareStatement("INSERT INTO event(sig_id, time) VALUES( ? ,FROM_UNIXTIME( ? ))"));
+    m_statement = m_conn->createStatement();
+/*
+    sql::SQLString str = "USE ";
+    str += dbName;
+    std::auto_ptr<sql::Statement> statement;
+    statement = m_conn->createStatement();
+    statement->execute(str);
+    delete statement;
+*/
 }
-CDB::~CDB()
+CDB::~CDB()//소멸자
 {
+    delete m_strEvent;
     delete m_statement;
     delete m_conn;
 }
-void CDB::connectDB(void)
+
+void CDB::logging(CPacket *packet, u_int32_t sig_id)//패킷과 룰 번호를 받아 db에 로그를 남김
 {
-    sql::SQLString str = "USE ";
-    str += m_dbName;
-    m_driver = sql::mysql::get_driver_instance();
-    m_conn = m_driver->connect(m_hostName, m_userName, m_password);
-    m_statement = m_conn->createStatement();
-    m_statement->execute(str);
+    //event table에 로그 저장
+    m_strEvent->setInt(1,sig_id);
+    m_strEvent->setInt(2,packet->time);
+    m_strEvent->execute();
+
+    //방금 남긴 로그의 eid를 가져옴
+    std::auto_ptr<sql::ResultSet> res;
+    m_statement->execute("SELECT MAX(eid) AS eid FROM event");
+    res.reset(m_statement->getResultSet());
+    res->next();
+    std::string eid=res->getString("eid");
+    delete res;
+/*
+    switch (packet->protocol_type)
+    {
+    case TCP:
+        //iphdr table에 저장 eid U_INT, src_ip  U_INT, dst_ip  U_INT, tos  U_TINYINT, ttl  U_TINYINT, more_frag   BOOLEAN, dont_frag   BOOLEAN,
+        snprintf(buff, 512, "INSERT INTO iphdr VALUES (%s, %u, %u, %u, %u, %s, %s)", 
+            eid, packet->tcp.getSrcIP(), packet->tcp.getDstIP(), packet->tcp.getTos(), packet->tcp.getTTL(), 
+            packet->tcp.getMoreFrag()?"true":"false", packet->tcp.getDontFrag()?"true":"false");
+        break;
+    case UDP:
+        break;
+    case ICMP:
+        break;
+    default:
+        break;
+    }
+    //iphdr table에 데이터 저장
+    
+*/
+    //std::cout<<eid<<std::endl;
+    /*
+    if(packet->protocol_type==TCP)
+    {
+
+    }
+    else if(packet->protocol_type==UDP)
+    {
+
+    }
+    else if(packet->protocol_type==ICMP)
+    {
+
+    }*/
+    //m_statement->execute("INSERT INTO test VALUES(1,2)");
 }
-void CDB::logging()
-{
-    m_statement->execute("INSERT INTO test VALUES(1,2)");
-}
-void CDB::getRule()
+void CDB::getRule()//db에서 룰을 가져옴
 {
     sql::ResultSet *res;
     res = m_statement->executeQuery("SELECT * FROM test");
--- HEADER/DB.h
+++ HEADER/DB.h
@@ -1,23 +1,29 @@
 #pragma once
 #include "ids.h"
+#include "Rule.h"
+#include "Packet.h"
 #include <mysql-cppconn-8/mysql/jdbc.h>
-
+#include <inttypes.h>
 class CDB
 {
 private:
-    sql::SQLString m_hostName;
-    sql::SQLString m_userName;
-    sql::SQLString m_dbName;
-    sql::SQLString m_password;
+    //sql::SQLString m_hostName; //db주소
+    //sql::SQLString m_userName; //아이디
+    //sql::SQLString m_dbName;   //db명
+    //sql::SQLString m_password; //비밀번호
     sql::mysql::MySQL_Driver *m_driver;
-    sql::Connection *m_conn;
-    sql::Statement *m_statement;
-
+    std::auto_ptr<sql::Connection> m_conn;
+    std::auto_ptr<sql::PreparedStatement> m_strEvent;
+    std::auto_ptr<sql::PreparedStatement> m_strIPhdr;
+    std::auto_ptr<sql::PreparedStatement> m_strTCPhdr;
+    std::auto_ptr<sql::PreparedStatement> m_strUDPhdr;
+    std::auto_ptr<sql::PreparedStatement> m_strICMPhdr;
+    std::auto_ptr<sql::PreparedStatement> m_strPayload;
+    std::auto_ptr<sql::Statement> m_statement;
 protected:
 public:
     CDB(sql::SQLString hostName, sql::SQLString userName, sql::SQLString password, sql::SQLString dbName);
     ~CDB();
-    void connectDB(void);
-    void logging(void);
+    void logging(CPacket *packet, u_int32_t sig_id);
     void getRule(void);
 };
