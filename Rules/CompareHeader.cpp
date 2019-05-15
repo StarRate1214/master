ad41ef990f0c2dfaacea38ae07faf85397015be9
StarRate1214 <kit@localhost.localdomain>
2019-05-15 17:58:22 +0900

    0515일 커밋

--- Rules/CompareHeader.cpp
+++ Rules/CompareHeader.cpp
@@ -1,43 +1,44 @@
-#include "../HEADER/RuleEngine.h"
+#include "RuleEngine.h"
 
 bool CRuleEngine::CompareHeader(CRule rule)
 {
-	int ruleProtocol;
-	u_int32_t packetSrcNetmask = 0;
-	u_int32_t packetDesNetmask = 0;
-	u_int16_t packetSrcPort = 0;
-	u_int16_t packetDesPort = 0;
+    int ruleProtocol;
+    u_int32_t packetSrcNetmask = 0;
+    u_int32_t packetDesNetmask = 0;
+    u_int16_t packetSrcPort = 0;
+    u_int16_t packetDesPort = 0;
 
-	//프로토콜 비교 후 분류
-	//동시에 들어온 패킷의 출발지, 목적지 IP의 netmask 변환.
-	switch (rule.GetProtocols())// Rule.h 에서 넘어온 string을 숫자로 바꿔줌
-	{
-	case TCP
-		ruleProtocol = TCP;
-		packetSrcNetmask = rule.GetSrcNetmask & packet.ip.getSrcIP();
-		packetDesNetmask = rule.GetDesNetmask & packet.ip.getDstIP();
-		packetSrcPort = packet.tcp.getSrcPort();
-		packetDesPort = packet.tcp.getDstPort();
-		break;
-	case UDP:
-		ruleProtocol = UDP;
-		packetSrcNetmask = rule.GetSrcNetmask & packet.ip.getSrcIP();
-		packetDesNetmask = rule.GetDesNetmask & packet.ip.getDstIP();
-		packetSrcPort = packet.udp.getSrcPort();
-		packetDesPort = packet.udp.getDstPort();
-		break;
-	case ICMP:
-		ruleProtocol = ICMP;
-		packetSrcNetmask = rule.GetSrcNetmask & packet.ip.getSrcIP();
-		packetDesNetmask = rule.GetDesNetmask & packet.ip.getDstIP();
-		break;
-	}
+    //프로토콜 비교 후 분류
+    //동시에 들어온 패킷의 출발지, 목적지 IP의 netmask 변환.
+    switch (rule.GetProtocols()) // Rule.h 에서 넘어온 string을 숫자로 바꿔줌
+    {
+    case TCP:
+        ruleProtocol = TCP;
+        packetSrcNetmask = rule.GetSrcNetmask() & packet.ip.getSrcIP();
+        packetDesNetmask = rule.GetDesNetmask() & packet.ip.getDstIP();
+        packetSrcPort = packet.tcp.getSrcPort();
+        packetDesPort = packet.tcp.getDstPort();
+        break;
+    case UDP:
+        ruleProtocol = UDP;
+        packetSrcNetmask = rule.GetSrcNetmask() & packet.ip.getSrcIP();
+        packetDesNetmask = rule.GetDesNetmask() & packet.ip.getDstIP();
+        packetSrcPort = packet.udp.getSrcPort();
+        packetDesPort = packet.udp.getDstPort();
+        break;
+    case ICMP:
+        ruleProtocol = ICMP;
+        packetSrcNetmask = rule.GetSrcNetmask() & packet.ip.getSrcIP();
+        packetDesNetmask = rule.GetDesNetmask() & packet.ip.getDstIP();
+        break;
+    }
 
-	if (ruleProtocol != packet.protocol_type) return false;//프로토콜 타입확인
+    if (ruleProtocol != packet.protocol_type)
+        return false; //프로토콜 타입확인
 
-		//ip방향성 확인
-	int headerdir;
-	if (rule.GetDirOperator() == "<>") headerdir = true;
+    //ip방향성 확인
+    int headerdir;
+    if (rule.GetDirOperator() == "<>") headerdir = true;
 	else headerdir = false;
 
 
@@ -358,42 +359,42 @@ bool CRuleEngine::CompareHeader(CRule rule)
 				{
 				case ANY: //넘어감
 					break;
-				case NOT: //
-					if (rule.GetDesPort() == packetSrcPort) return false;
+				case NOT: //제외
+					if (PortCompare(rule.GetDesPort(), packetSrcPort)==true) return false;
 					break;
-				case COMM: //단일 PORT
-					if (rule.GetDesPort() != packetSrcPort) return false;
+				case COMM: // PORT
+					if (PortCompare(rule.GetDesPort(), packetSrcPort)==false) return false;
 					break;
 				}
 				break;
 			case NOT: //
-				if (rule.GetSrcPort() == packetSrcPort)
+				if (PortCompare(rule.GetSrcPort(), packetSrcPort)==true)
 				{
 					switch (rule.GetDesPOpt())//역방향 비교
 					{
 					case ANY: //넘어감
 						break;
 					case NOT: //
-						if (rule.GetDesPort() == packetSrcPort) return false;
+						if (PortCompare(rule.GetDesPort(), packetSrcPort)==true) return false;
 						break;
 					case COMM: //단일 PORT
-						if (rule.GetDesPort() != packetSrcPort) return false;
+						if (PortCompare(rule.GetDesPort(), packetSrcPort)==false) return false;
 						break;
 					}
 				}
 				break;
 			case COMM: //단일 PORT
-				if (rule.GetSrcPort() != packetSrcPort)
+				if (PortCompare(rule.GetSrcPort(), packetSrcPort)==false)
 				{
 					switch (rule.GetDesPOpt())//역방향 비교
 					{
 					case ANY: //넘어감
 						break;
 					case NOT: //
-						if (rule.GetDesPort() == packetSrcPort) return false;
+						if (PortCompare(rule.GetDesPort(), packetSrcPort)==true) return false;
 						break;
 					case COMM: //단일 PORT
-						if (rule.GetDesPort() != packetSrcPort) return false;
+						if (PortCompare(rule.GetDesPort(), packetSrcPort)==false) return false;
 						break;
 					}
 				}
@@ -409,41 +410,41 @@ bool CRuleEngine::CompareHeader(CRule rule)
 				case ANY: //넘어감
 					break;
 				case NOT: //
-					if (rule.GetSrcPort() == packetDesPort) return false;
+					if (PortCompare(rule.GetSrcPort(), packetDesPort)==true) return false;
 					break;
 				case COMM: //단일 PORT
-					if (rule.GetSrcPort() != packetDesPort) return false;
+					if (PortCompare(rule.GetSrcPort(), packetDesPort)==false) return false;
 					break;
 				}
 				break;
 			case NOT: //
-				if (rule.GetDesPort() == packetDesPort)
+				if (PortCompare(rule.GetDesPort(), packetDesPort)==true)
 				{
 					switch (rule.GetSrcPOpt())
 					{
 					case ANY: //넘어감
 						break;
 					case NOT: //
-						if (rule.GetSrcPort() == packetDesPort) return false;
+						if (PortCompare(rule.GetSrcPort(), packetDesPort)==true) return false;
 						break;
 					case COMM: //단일 PORT
-						if (rule.GetSrcPort() != packetDesPort) return false;
+						if (PortCompare(rule.GetSrcPort(), packetDesPort)==false) return false;
 						break;
 					}
 				}
 				break;
 			case COMM: //단일 PORT
-				if (rule.GetDesPort() != packetDesPort)
+				if (PortCompare(rule.GetDesPort(), packetDesPort)==false)
 				{
 					switch (rule.GetSrcPOpt())
 					{
 					case ANY: //넘어감
 						break;
 					case NOT: //
-						if (rule.GetSrcPort() == packetDesPort) return false;
+						if (PortCompare(rule.GetSrcPort(), packetDesPort)==true) return false;
 						break;
 					case COMM: //단일 PORT
-						if (rule.GetSrcPort() != packetDesPort) return false;
+						if (PortCompare(rule.GetSrcPort(), packetDesPort)==false) return false;
 						break;
 					}
 				}
@@ -461,41 +462,41 @@ bool CRuleEngine::CompareHeader(CRule rule)
 				case ANY: //넘어감
 					break;
 				case NOT: //
-					if (rule.GetDesPort() == packetSrcPort) return false;
+					if (PortCompare(rule.GetDesPort(), packetSrcPort)==true) return false;
 					break;
 				case COMM: //단일 PORT
-					if (rule.GetDesPort() != packetSrcPort) return false;
+					if (PortCompare(rule.GetDesPort(), packetSrcPort)==false) return false;
 					break;
 				}
 				break;
 			case NOT: //
-				if (rule.GetSrcPort() == packetSrcPort)
+				if (PortCompare(rule.GetSrcPort(), packetSrcPort)==true)
 				{
 					switch (rule.GetDesPOpt())//출발지 PORT 역방향 비교
 					{
 					case ANY: //넘어감
 						break;
 					case NOT: //
-						if (rule.GetDesPort() == packetSrcPort) return false;
+						if (PortCompare(rule.GetDesPort(), packetSrcPort)==true) return false;
 						break;
 					case COMM: //단일 PORT
-						if (rule.GetDesPort() != packetSrcPort) return false;
+						if (PortCompare(rule.GetDesPort(), packetSrcPort)==false) return false;
 						break;
 					}
 				}
 				break;
 			case COMM: //단일 PORT
-				if (rule.GetSrcPort() != packetSrcPort)
+				if (PortCompare(rule.GetSrcPort(), packetSrcPort)==false)
 				{
 					switch (rule.GetDesPOpt())//출발지 PORT 역방향 비교
 					{
 					case ANY: //넘어감
 						break;
 					case NOT: //
-						if (rule.GetDesPort() == packetSrcPort) return false;
+						if (PortCompare(rule.GetDesPort(), packetSrcPort)==true) return false;
 						break;
 					case COMM: //단일 PORT
-						if (rule.GetDesPort() != packetSrcPort) return false;
+						if (PortCompare(rule.GetDesPort(), packetSrcPort)==false) return false;
 						break;
 					}
 				}
@@ -511,41 +512,41 @@ bool CRuleEngine::CompareHeader(CRule rule)
 				case ANY: //넘어감
 					break;
 				case NOT: //
-					if (rule.GetSrcPort() == packetDesPort) return false;
+					if (PortCompare(rule.GetSrcPort(), packetDesPort)==true) return false;
 					break;
 				case COMM: //단일 PORT
-					if (rule.GetSrcPort() != packetDesPort) return false;
+					if (PortCompare(rule.GetSrcPort(), packetDesPort)==false) return false;
 					break;
 				}
 				break;
 			case NOT: //
-				if (rule.GetDesPort() == packetDesPort)
+				if (PortCompare(rule.GetDesPort(), packetDesPort)==true)
 				{
 					switch (rule.GetSrcPOpt())//목적지 PORT 역방향 비교
 					{
 					case ANY: //넘어감
 						break;
 					case NOT: //
-						if (rule.GetSrcPort() == packetDesPort) return false;
+						if (PortCompare(rule.GetSrcPort(), packetDesPort)==true) return false;
 						break;
 					case COMM: //단일 PORT
-						if (rule.GetSrcPort() != packetDesPort) return false;
+						if (PortCompare(rule.GetSrcPort(), packetDesPort)==false) return false;
 						break;
 					}
 				}
 				break;
 			case COMM: //단일 PORT
-				if (rule.GetDesPort() != packetDesPort)
+				if (PortCompare(rule.GetDesPort(), packetDesPort)==false)
 				{
 					switch (rule.GetSrcPOpt())//목적지 PORT 역방향 비교
 					{
 					case ANY: //넘어감
 						break;
 					case NOT: //
-						if (rule.GetSrcPort() == packetDesPort) return false;
+						if (PortCompare(rule.GetSrcPort(), packetDesPort)==true) return false;
 						break;
 					case COMM: //단일 PORT
-						if (rule.GetSrcPort() != packetDesPort) return false;
+						if (PortCompare(rule.GetSrcPort(), packetDesPort)==false) return false;
 						break;
 					}
 				}
@@ -568,10 +569,10 @@ bool CRuleEngine::CompareHeader(CRule rule)
 			case ANY: //넘어감
 				break;
 			case NOT: //
-				if (rule.GetSrcPort() == packetSrcPort) return false;
+				if (PortCompare(rule.GetSrcPort(), packetSrcPort)==true) return false;
 				break;
 			case COMM: //단일 PORT
-				if (rule.GetSrcPort() != packetSrcPort) return false;
+				if (PortCompare(rule.GetSrcPort(), packetSrcPort)==false) return false;
 				break;
 			}
 
@@ -580,10 +581,10 @@ bool CRuleEngine::CompareHeader(CRule rule)
 			case ANY: //넘어감
 				break;
 			case NOT: //
-				if (rule.GetDesPort() == packetDesPort) return false;
+				if (PortCompare(rule.GetDesPort(), packetDesPort)==true) return false;
 				break;
 			case COMM: //단일 PORT
-				if (rule.GetDesPort() != packetDesPort) return false;
+				if (PortCompare(rule.GetDesPort(), packetDesPort)==false) return false;
 				break;
 			}
 			break;
@@ -594,10 +595,10 @@ bool CRuleEngine::CompareHeader(CRule rule)
 			case ANY: //넘어감
 				break;
 			case NOT: //
-				if (rule.GetSrcPort() == packetSrcPort) return false;
+				if (PortCompare(rule.GetSrcPort(), packetSrcPort)==true) return false;
 				break;
 			case COMM: //단일 PORT
-				if (rule.GetSrcPort() != packetSrcPort) return false;
+				if (PortCompare(rule.GetSrcPort(), packetSrcPort)==false) return false;
 				break;
 			}
 
@@ -606,41 +607,34 @@ bool CRuleEngine::CompareHeader(CRule rule)
 			case ANY: //넘어감
 				break;
 			case NOT: //
-				if (rule.GetDesPort() == packetDesPort) return false;
+				if (PortCompare(rule.GetDesPort(), packetDesPort)==true) return false;
 				break;
 			case COMM: //단일 PORT
-				if (rule.GetDesPort() != packetDesPort) return false;
+				if (PortCompare(rule.GetDesPort(), packetDesPort)==false) return false;
 				break;
 			}
 			break;
 
 		case ICMP://단방향 ICMP PORT비교
-			switch (rule.GetSrcPOpt())//출발지 PORT
-			{
-			case ANY: //넘어감
-				break;
-			case NOT: //
-				if (rule.GetSrcPort() == packetSrcPort) return false;
-				break;
-			case COMM: //단일 PORT
-				if (rule.GetSrcPort() != packetSrcPort) return false;
-				break;
-			}
-
-			switch (rule.GetDesPOpt())//목적지 PORT
-			{
-			case ANY: //넘어감
-				break;
-			case NOT: //
-				if (rule.GetDesPort() == packetDesPort) return false;
-				break;
-			case COMM: //단일 PORT
-				if (rule.GetDesPort() != packetDesPort) return false;
-				break;
-			}
 			break;
 		}
 		break;
 	}
 	return true;
+}
+
+bool PortCompare(std::vector<u_int16_t> rulePort, u_int16_t packetPort)
+{
+    if (rulePort.size()==1)
+    {
+       if (rulePort[0]== packetPort) return true;
+    }
+    else
+    {
+         for (int i = rulePort[0];  i < rulePort[1]; i++)
+        {
+            if (i == packetPort) return true;
+        }
+    }
+    return false;
 }
