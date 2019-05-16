e345af93dff468b04810415fcae4fd32ab6edd68
Lrsug <observer@localhost.localdomain>
2019-05-16 17:54:20 +0900

    ruleEngine(CRule) main으로 변경

--- HEADER/RuleEngine.h
+++ HEADER/RuleEngine.h
@@ -8,13 +8,12 @@ class CRuleEngine : public CNonPayload //load, compare
 {
 private:
     CPacket packet;
-    std::vector<CRule> rules;
     bool content(std::string content, int semicolon, bool nocase, int depth, int offset, int distance, int within, u_int8_t http_option);
     bool pcre(std::string option);
     bool compareHeader(CRule);
     bool CompareOption(std::vector<SRule_option> options);
 public:
-    void RuleLoad(std::string rule_fileName);
+    void RuleLoad(std::string rule_fileName,std::vector<CRule> rules);
     void PacketLoad(u_int8_t *buff);
     int Compare();
 };
