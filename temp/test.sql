219ae57cd7f67491315d3ebf1607fcf70a7ddd76
lrsug628 <49192741+lrsug628@users.noreply.github.com>
2019-05-31 16:27:50 +0900

    Update test.sql

--- temp/test.sql
+++ temp/test.sql
@@ -10,6 +10,42 @@ INSERT INTO signature  VALUES(
     "(msg:"in-out udp"; sid: 1000021;)"#룰 옵션, general rule option은 제거
 );
 
+INSERT INTO signature(sig_msg,sig_rev,sig_sid,sig_gid,sig_rule_header,sig_rule_option) VALUES( 
+    'test_rule_content:"http"; nocase;',#룰 이름
+    1,#룰 버전
+    100,#룰 고유번호
+    1,#룰 그룹 번호
+    "alert tcp any any -> any 80",#룰 헤더
+    'content:"http"; nocase;'#룰 옵션, general rule option은 제거
+);
+
+INSERT INTO signature(sig_msg,sig_rev,sig_sid,sig_gid,sig_rule_header,sig_rule_option) VALUES( 
+    "test_rule_content:!"asdf"; http_cookie;",#룰 이름
+    1,#룰 버전
+    101,#룰 고유번호
+    1,#룰 그룹 번호
+    "alert tcp any any -> any 80",#룰 헤더
+    'content:!"asdf"; http_cookie; '#룰 옵션, general rule option은 제거
+);
+
+INSERT INTO signature(sig_msg,sig_rev,sig_sid,sig_gid,sig_rule_header,sig_rule_option) VALUES( 
+    'test_rule_content:"|48 54 54 50|";',#룰 이름
+    1,#룰 버전
+    102,#룰 고유번호
+    1,#룰 그룹 번호
+    "alert tcp any any -> any 80",#룰 헤더
+    'content:"|48 54 54 50|";'#룰 옵션, general rule option은 제거
+);
+
+INSERT INTO signature(sig_msg,sig_rev,sig_sid,sig_gid,sig_rule_header,sig_rule_option) VALUES( 
+    'test_rule_content:"|48 54 54 50|";',#룰 이름
+    1,#룰 버전
+    102,#룰 고유번호
+    1,#룰 그룹 번호
+    "alert tcp any any -> any 80",#룰 헤더
+    'content:"http"; pcre:"aaa/i";'#룰 옵션, general rule option은 제거
+);
+
 #SELECT * FROM event;
 #SELECT * FROM iphdr;
 #SELECT * FROM signature;
