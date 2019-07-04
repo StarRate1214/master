e6c1ca6818c10a5ec3ee5925c43f0441046f1e1c
BeomJinKim <project@localhost.localdomain>
2019-07-04 19:53:53 +0900

    카운트 옵션

--- Rules/Count.cpp
+++ Rules/Count.cpp
@@ -1,1 +1,37 @@
-#include "count.h"
+#include "Count.h"
+
+CCount::CCount(u_int32_t sig_id, u_int8_t rev, int limit, time_t timeout)
+    :sig_id(sig_id), rev(rev), limit(limit), timeout(timeout)
+{
+    
+}
+
+CCount &CCount::operator=(CCount &ref)
+{
+    this->rev = ref.rev;
+    this->sig_id = ref.sig_id;
+
+    this->timeout = ref.timeout;
+    this->limit = ref.limit;
+
+    this->timeInfo = ref.timeInfo;
+    this->packet = ref.packet;
+}
+
+CCount::CCount(CCount &ref)
+    :sig_id(ref.sig_id), rev(ref.rev), limit(ref.limit), timeout(ref.timeout)
+{
+    this->timeInfo = ref.timeInfo;
+    this->packet = ref.packet;
+}
+
+int CCount::getPacketCount(void)
+{
+    return packet.size();
+}
+
+void CCount::insertPacket(CPacket& packet)
+{
+    this->timeInfo.push_back(packet.time);
+    this->packet.push_back(packet);
+}
