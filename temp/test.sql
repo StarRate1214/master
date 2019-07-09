cca000e41a341f1aad5beb63b138489cb429c2c1
jwh <ben3329@naver.com>
2019-07-09 19:27:13 +0900

    m

--- temp/test.sql
+++ temp/test.sql
@@ -2,7 +2,7 @@ use test;
 #SELECT * FROM event;
 #SELECT * FROM iphdr;
 
-#DELETE FROM signature;
+DELETE FROM signature;
 
 INSERT INTO signature(sig_msg,sig_rev,sig_sid,sig_gid,sig_rule_header,sig_rule_option) VALUES( 
     'test_rule_content:"http"; nocase;',#룰 이름
@@ -10,7 +10,7 @@ INSERT INTO signature(sig_msg,sig_rev,sig_sid,sig_gid,sig_rule_header,sig_rule_o
     100,#룰 고유번호
     1,#룰 그룹 번호
     "alert icmp any any -> any any",#룰 헤더
-    'count:3/5'#룰 옵션, general rule option은 제거
+    'count:3/5;'#룰 옵션, general rule option은 제거
 );
 
 SELECT * FROM signature;
