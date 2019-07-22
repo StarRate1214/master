a1d44110cdd819cb722b17605b9b2e091a34729e
CielTMP <49310205+CielTMP@users.noreply.github.com>
2019-07-22 12:12:23 +0900

    Update RuleEngine.h

--- HEADER/RuleEngine.h
+++ HEADER/RuleEngine.h
@@ -16,14 +16,13 @@ private:
     char * c_payload;
     char * c_httpmatch;
     CPacket packet;
-    // TEST!! int content(std::string cnt, bool nocase, int depth, int offset, int distance, int within, u_int8_t http_option, int prev);
+    int content(std::string cnt, bool nocase, int depth, int offset, int distance, int within, u_int8_t http_option, int prev);
     bool CompareOption(std::vector<SRule_option> options, CNation *country);
     bool pcre(std::string option);
     std::vector<CDetection_filter> d_filter;
 public:
     CRuleEngine();
     ~CRuleEngine();
-    int content(std::string cnt, bool nocase, int depth, int offset, int distance, int within, u_int8_t http_option, int prev);
     //void RuleLoad(std::string rule_fileName,std::vector<CRule> rules);
     void PacketLoad(CRawpacket *rwpack);
     int Compare(std::vector<CRule> *rules, CNation *country , int ruleNum = 0);
