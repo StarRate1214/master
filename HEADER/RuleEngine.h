17dad55d096625a95c03a897903ef9971b78f50f
ciels.office@gmail.com <msh@localhost.localdomain>
2019-07-21 22:57:27 -0400

    Update RuleEngine

--- HEADER/RuleEngine.h
+++ HEADER/RuleEngine.h
@@ -12,14 +12,18 @@ class CRuleEngine : public CNonPayload, public CCompareHeader //load, compare
 {
 private:
     char * hexcontent;
+    char * c_content;
+    char * c_payload;
+    char * c_httpmatch;
     CPacket packet;
-    int content(std::string cnt, bool nocase, int depth, int offset, int distance, int within, u_int8_t http_option, int prev);
+    // TEST!! int content(std::string cnt, bool nocase, int depth, int offset, int distance, int within, u_int8_t http_option, int prev);
     bool CompareOption(std::vector<SRule_option> options, CNation *country);
     bool pcre(std::string option);
     std::vector<CDetection_filter> d_filter;
 public:
     CRuleEngine();
     ~CRuleEngine();
+    int content(std::string cnt, bool nocase, int depth, int offset, int distance, int within, u_int8_t http_option, int prev);
     //void RuleLoad(std::string rule_fileName,std::vector<CRule> rules);
     void PacketLoad(CRawpacket *rwpack);
     int Compare(std::vector<CRule> *rules, CNation *country , int ruleNum = 0);
