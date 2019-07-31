6cbda5319dc1d5c1b0bef2e98782ccf37b68a3b9
jwh <ben3329@naver.com>
2019-07-31 10:38:54 +0900

    delete account table

--- DB/logschema.sql
+++ DB/logschema.sql
@@ -152,14 +152,4 @@ CREATE TABLE data   (
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
-
-#Account Table
-CREATE TABLE account (
-  `u_num` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `u_id` VARCHAR(255) NOT NULL, #userid
-  `u_pw` VARCHAR(255) NOT NULL, #userpw
-  `u_active` TINYINT(1) NOT NULL DEFAULT 1, #유저 활성 여부
-  `u_update` TINYINT(1) NOT NULL DEFAULT 0, #유저 변경 권한 여부
-  `r_update` TINYINT(1) NOT NULL DEFAULT 0, #룰 추가/수정/삭제 가능 여부
-  PRIMARY KEY (`u_num`));
  
